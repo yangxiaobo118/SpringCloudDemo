34e1381e24c01eb8cc0fd758fe3ce06c23cacca2
yangxiaobo <380757072@qq.com>
2018-01-17 10:14:30 +0800

    001

--- spring-clound-client/src/main/java/com/suke/ServiceHiApplication.java
+++ spring-clound-client/src/main/java/com/suke/ServiceHiApplication.java
@@ -4,13 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServiceHiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHiApplication.class, args);
@@ -18,7 +24,11 @@ public class ServiceHiApplication {
 	@Value("${server.port}")
 	String port;
 	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod="hiError")
 	public String home(@RequestParam String name) {
 		return "hi,"+name+",i am from : "+port;
+	}
+	public String hiError(@RequestParam String name) {
+		return "hi,"+name+"sorry,error!";
 	}
 }
